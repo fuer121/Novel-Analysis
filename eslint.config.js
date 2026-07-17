10f2f587b99467f7073cc537f1906a9a07f71409
fuer121 <hushaobin21@gmail.com>
2026-07-17 12:46:05 +0800

    ci: verify legacy and new architecture contracts

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'data']),
+  globalIgnores(['dist', 'data', 'node_modules', '**/*.tsbuildinfo']),
   {
     files: ['src/**/*.{js,jsx}'],
     extends: [
@@ -26,6 +27,22 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['packages/**/*.ts'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: globals.node,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
   },
 ])
